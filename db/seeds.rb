9a20ff516e9021093debdce8abbca5e388fb90f9
shangweilin <shangweilin7@gmail.com>
2017-04-30 21:27:03 +0800

    create db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+photo_list = [
+  { title: "Myra's birthday", date: "2013-02-02", description: "This is my favourite photo of Myra's birthday. It was a wonderful day.", file_location: "my-photo/myras-birthday-20130202.jpg" },
+  { title: "Family outing", date: "2014-11-13", description: "Spending some time at the park after a long week.", file_location: "my-photo/family-outing-20141113.jpg" },
+  { title: "Anniversary", date: "2010-11-22", description: "This is  our 2nd anniversary.", file_location: "my-photo/my-anniversary-20101122.jpg" }
+]
+
+photo_list.each do |photo|
+  Photo.create(title: photo[:title], date: photo[:date], description: photo[:description], file_location: photo[:description])
+end
