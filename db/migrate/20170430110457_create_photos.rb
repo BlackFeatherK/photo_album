1c8e7aee2a8229c33a72c91894dfd799f330aab9
shangweilin <shangweilin7@gmail.com>
2017-04-30 21:26:25 +0800

    generrate photo model

---
+++ db/migrate/20170430110457_create_photos.rb
@@ -0,0 +1,15 @@
+class CreatePhotos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :photos do |t|
+      t.string :title
+      t.string :date
+      t.string :date
+      t.text :description
+      t.string :file_location
+      t.boolean :is_public
+      t.integer :capacity
+
+      t.timestamps
+    end
+  end
+end
